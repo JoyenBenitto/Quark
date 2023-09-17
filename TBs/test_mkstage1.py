83cec3c63929acb4d05ebd9fc64e865205a8c0ce
Joyen Benitto <JoyenBenitto>
2023-09-17 10:31:53 +0530

    init test

---
+++ TBs/test_mkstage1.py
@@ -0,0 +1,15 @@
+import cocotb
+from cocotb.triggers import Timer, RisingEdge
+
+@cocotb.test()
+async def mkstage1_test(dut):
+    a=(0,0,1,1)
+    b=(0,1,0,1)
+    y=(0,1,1,1)
+    
+    for i in range(4):
+        dut.a.value = a[i]
+        dut.b.value = b[i]
+        await Timer(1, 'ns')
+        assert dut.y.value == y[i], f"Error at iteration {i}" 
+    pass
---
+++ build/verilog/or_gate.v
@@ -0,0 +1,8 @@
+module or_gate(
+    input wire a,
+    input wire b,
+    output wire y
+);
+
+assign y  = a|b;
+endmodule
